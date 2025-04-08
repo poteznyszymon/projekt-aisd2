6afbe7fef9da2fbe9944d22aa56b8719062b5eda
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2025-04-08 10:29:13 +0200

    update mapy

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ DATA_DIR_PATH = os.path.join(os.path.dirname(__file__), "data")
 
 def main():
     with open(os.path.join(DATA_DIR_PATH, "fields.json"), "r") as f:
-        fields = json.load(f)
-    print(fields['fields'][0])
+        fields_list = json.load(f)['fields']
+    print(fields_list)
 
 if __name__ == "__main__":
     main()
